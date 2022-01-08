34d2db6235740c6edeb899ddb3786f670f8a2a30
Drackin Best <78258229+Drackin@users.noreply.github.com>
2022-01-09 00:01:49 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
 const WindiCSSWebpackPlugin = require('windicss-webpack-plugin')
 
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`,
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+}
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   webpack: (config) => {
@@ -12,6 +23,10 @@ module.exports = {
     return config
   },
   images: {
-    disableStaticImages: true
+    disableStaticImages: true,
+    domains: [
+      "avatars.githubusercontent.com",
+      "localhost"
+    ]
   }
 }
