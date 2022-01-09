e0d01a33b1e76fab08f6c7e2d5f940c8316496d8
DrackinBest <sonaycannet10@gmail.com>
2022-01-09 20:16:45 +0300

    Font Support

--- pages/api/twitter.ts
+++ pages/api/twitter.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { createCanvas, loadImage } from "canvas"
+import { createCanvas, loadImage, registerFont } from "canvas"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const { avatar, content, fullname, username } = (req as any).query;
@@ -8,20 +8,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         return res.status(400).json({ code: 400, message: "Please Enter All Query Params. (avatar, content, fullname, username)" })
     }
 
+    registerFont("./assets/Roboto-Regular.ttf", {
+      family: "Roboto"
+    })
     const canvas = createCanvas(596, 334);
     const ctx = canvas.getContext("2d");
     // Load Background Image
     const img = await loadImage("https://i.imgur.com/jBG4QUn.png");
     ctx.drawImage(img, 0, 0, 596, 334);
     // Content
-    ctx.font = "25px Roboto";
+    ctx.font = '25px "Roboto"';
     ctx.fillStyle = "white"
     wrapText(ctx, content, 10, 100, 580, 30)
     // Full Name
-    ctx.font = "bold 25px Arial";
+    ctx.font = 'bold 25px "Roboto"';
     ctx.fillText(fullname, 72, 35);
     // Username
-    ctx.font = "15px Roboto";
+    ctx.font = '15px "Roboto"';
     ctx.fillStyle = "#AAB8C2";
     ctx.fillText(username, 86, 54);
     // Profile Photo
