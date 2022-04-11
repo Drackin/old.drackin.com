54219d8ddb8f4030b605a2b6fe84d7351da7f12b
Drackin Best <sonaycannet10@gmail.com>
2022-04-11 21:16:17 +0300

    meta Tags Added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,10 +24,15 @@ const Home: NextPage = () => {
     <div className="max-w-full min-h-screen bg-neutral-900 text-white max-w-">
       <Head>
         <title>Drackin</title>
-        <meta name="description" content="Hello, I'm Drackin. This is My Personal Website. I Love Coding." />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="description" content="Hello!
+        I'm Drackin. I'm a Developer & Violinist & Athlete. This is My Personal Website. I Love Coding." property="og:description"/>
+        <meta content="Drackin's Website" property="og:title" />
+        <meta content="drackin.me" property="og:site_name" />
         <link rel="icon" href="https://avatars.githubusercontent.com/u/78258229?v=4" />
         <meta name="theme-color" content="#3b82f6" />
         <meta name="author" content="Drackin Best" />
+        <meta content="https://i.imgur.com/qDSu23l.jpg" itemProp="image" property="og:image" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       {isLoading === true ?
